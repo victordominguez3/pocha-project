a2d1c516e9638f59a04d8eec2c03f1d1b438439c
Víctor Domínguez <victor.dominguez@alumno.iesluisvives.org>
2024-12-23 12:59:00 +0100

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "mi-pwa-cache-v1";
+const urlsToCache = [
+  "./",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/apple-touch-icon.png",
+  "/android-chrome-512x512.png"
+];
+
+// Instalar el Service Worker y guardar en caché
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Interceptar solicitudes y servir desde la caché
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      // Opcional: Proporciona un archivo de respaldo si todo falla
+      return caches.match('./index.html');
+  })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cacheName) => {
+                  if (!cacheWhitelist.includes(cacheName)) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
+  );
+});
